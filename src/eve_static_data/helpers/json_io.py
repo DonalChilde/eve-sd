e1a869ad9a76c3d03a206b9d7377187d72b25ca9
Chad Lowe <pfmsoft.dev@gmail.com>
2026-06-16 04:21:11 -0700

    refactor: enhance json dumping functions to support indentation and improve file handling

--- src/eve_static_data/helpers/json_io.py
+++ src/eve_static_data/helpers/json_io.py
@@ -16,18 +16,51 @@ def json_loads(json_string: str | bytes) -> Any:
     return from_json(json_string)
 
 
-def json_dumps(obj: Any, **kwargs: Any) -> str:
+def json_dumps(obj: Any, indent: int | None = None, **kwargs: Any) -> str:
     """Dump a Python object to a JSON string."""
-    return to_json(obj, **kwargs).decode("utf-8")
+    return to_json(obj, indent=indent, **kwargs).decode("utf-8")
 
 
-def json_dump_bytes(obj: Any, **kwargs: Any) -> bytes:
+def json_dump_bytes(obj: Any, indent: int | None = None, **kwargs: Any) -> bytes:
     """Dump a Python object to a JSON bytes."""
-    return to_json(obj, **kwargs)
+    return to_json(obj, indent=indent, **kwargs)
 
 
-def json_dump_path(obj: Any, *, filepath: Path, **kwargs: Any) -> None:
-    """Dump a Python object to a JSON file."""
+def json_dump_path(
+    obj: Any,
+    *,
+    filepath: Path,
+    overwrite: bool = False,
+    indent: int | None = None,
+    **kwargs: Any,
+) -> int:
+    """Dump a Python object to a JSON file.
+
+    Uses Pydantic's `to_json` function to serialize the object.
+
+    Args:
+        obj: The Python object to dump.
+        filepath: The path to the JSON file.
+        overwrite: Whether to overwrite the file if it exists.
+        indent: The indentation level for the JSON file.
+        **kwargs: Additional keyword arguments passed to `json_dumps`.
+
+    Returns:
+        The number of characters written to the file.
+
+    Raises:
+        FileExistsError: If the file already exists and `overwrite` is False.
+    """
     filepath.parent.mkdir(parents=True, exist_ok=True)
-    json_string = json_dumps(obj, **kwargs)
-    filepath.write_text(json_string, encoding="utf-8")
+    if overwrite:
+        with filepath.open("w", encoding="utf-8") as f:
+            counter = 0
+            counter += f.write(json_dumps(obj, indent=indent, **kwargs))
+            counter += f.write("\n")
+            return counter
+    else:
+        with filepath.open("x", encoding="utf-8") as f:
+            counter = 0
+            counter += f.write(json_dumps(obj, indent=indent, **kwargs))
+            counter += f.write("\n")
+            return counter
